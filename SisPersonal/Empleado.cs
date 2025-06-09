2b73cf1f26388264bbef20484ebbd1e3e410b188
Fabri <fabricioclaudio1@gmail.com>
2025-06-09 18:53:21 -0300

    Agrego los metodos a la Clase Empleado

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -2,24 +2,27 @@ namespace EspacioEmpleado
 {
     public class Empleado
     {
-        private string nombre;
-        private string apellido;
+        private string? nombre;
+        private string? apellido;
         private DateTime fechaDeNacimiento;
         private char estadoCivil;
         private DateTime fechaDeIngresoEnLaEmpresa;
         private double sueldoBasico;
         private Cargos cargo;
 
-        public string Nombre { get => nombre; set => nombre = value; }
+        public Empleado(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
 
-        public string Apellido { get => apellido; set => apellido = value; }
-
-        public DateTime Fechadenacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
+        public string? Nombre { get => nombre; set => nombre = value; }
+        public string? Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaDeNacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
         public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
         public DateTime FechaDeIngresoEnLaEmpresa { get => fechaDeIngresoEnLaEmpresa; set => fechaDeIngresoEnLaEmpresa = value; }
         public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
         public Cargos Cargo { get => cargo; set => cargo = value; }
-
 
         public enum Cargos
         {
@@ -29,6 +32,81 @@ namespace EspacioEmpleado
             Especialista,
             Investigador
         }
+        public int Antiguedad()
+        {
+            DateTime hoy = DateTime.Today;
+            int antiguedad = hoy.Year - FechaDeIngresoEnLaEmpresa.Year;
+
+            if (FechaDeIngresoEnLaEmpresa.Date > hoy.AddYears(-antiguedad))
+            {
+                antiguedad--;
+            }
+
+            return antiguedad;
+        }
+
+        public int Edad()
+        {
+            DateTime hoy = DateTime.Today;
+            int edad = hoy.Year - FechaDeNacimiento.Year;
+
+            if (FechaDeNacimiento < hoy.AddYears(-edad))
+            {
+                edad--;
+            }
+
+            return edad;
+        }
+
+        public int AniosParaJubilarse(int edadJubilacion)
+        {
+            DateTime hoy = DateTime.Today;
+            DateTime fechaDeJubilacion = fechaDeNacimiento.AddYears(edadJubilacion);
+            int aniosParaJubilarse = fechaDeJubilacion.Year - hoy.Year;
+
+            if (hoy < fechaDeJubilacion.AddYears(-aniosParaJubilarse))
+            {
+                aniosParaJubilarse--;
+            }
+
+            return aniosParaJubilarse;
+        }
+
+        public double Salario()
+        {
+            double adicional = SueldoBasico;
+
+            int antiguedad = Antiguedad();
+
+            if (1 <= antiguedad && antiguedad <= 20)
+            {
+                adicional += SueldoBasico * 0.01;
+            }
+            else
+            {
+                if (Antiguedad()>20)
+                {
+                    adicional += SueldoBasico * 0.25;
+                }
+            }
+
+            if (this.Cargo == Cargos.Ingeniero || this.Cargo == Cargos.Especialista)
+            {
+                adicional += adicional * 0.5;
+            }
+
+            if (this.EstadoCivil == 'C')
+            {
+                adicional += 150000;
+            }
+
+
+            
+
+
+            return SueldoBasico + adicional;
+        }
+
 
         
     }
